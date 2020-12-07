c9ce2d15d51ec2cbdf8577ede0ed362c77ab611c
Seth Phillips <seth@seth-phillips.com>
2020-12-07 12:46:21 -0500

    Added dynamic settings page

--- includes/glp-visualizer.class.php
+++ includes/glp-visualizer.class.php
@@ -2,14 +2,127 @@
 
 class GLPVisualizer {
 	
+	private $glp_visualizer_options;
 	protected $shinyserver;
 	protected $downloadable;
 	
 	public function __construct() {
-		$this->shinyserver = 'https://greaterlou.shinyapps.io/';
-		$this->downloadable = get_home_url().'/uofl-plugin-testing-form/'; // TODO
+		$this->glp_visualizer_options = get_option('glp_visualizer_option_name');
+		$this->initOptions();
+		
+		$this->shinyserver = $this->glp_visualizer_options['shiny_server'];
+		$this->downloadable = get_home_url().$this->glp_visualizer_options['form_location'];
+		
+		$this->initAdmin();
 		$this->loadScripts();
 		$this->bindShortcodes();
+	}
+	
+	private function checkInvalid($str) {
+		return $this->glp_visualizer_options === false || $this->glp_visualizer_options[$str] === false;
+	}
+	
+	private function initOptions() {
+		if($this->checkInvalid($this->glp_visualizer_options['shiny_server'])) {
+			$this->glp_visualizer_options['shiny_server'] = 'https://greaterlou.shinyapps.io/';
+			update_option('glp_visualizer_option_name', $this->glp_visualizer_options);
+		}
+		if($this->checkInvalid($this->glp_visualizer_options['form_location'])) {
+			$this->glp_visualizer_options['form_location'] = '/path-to-form/';
+			update_option('glp_visualizer_option_name', $this->glp_visualizer_options);
+		}
+	}
+	
+	private function initAdmin() {
+		add_action('admin_menu', array($this, 'glp_visualizer_add_plugin_page'));
+		add_action('admin_init', array($this, 'glp_visualizer_page_init'));
+	}
+	
+	public function glp_visualizer_add_plugin_page() {
+		add_menu_page(
+			'GLP Visualizer', // page_title
+			'GLP Visualizer', // menu_title
+			'manage_options', // capability
+			'glp-visualizer', // menu_slug
+			array($this, 'glp_visualizer_create_admin_page'), // function
+			'dashicons-media-spreadsheet', // icon_url
+			81 // position
+		);
+	}
+	
+	public function glp_visualizer_create_admin_page() {
+		$this->glp_visualizer_options = get_option('glp_visualizer_option_name');
+		?>
+		<div class="wrap">
+			<h2>GLP Visualizer</h2>
+			<p>Manage settings for the GLP Visualizer plugin</p>
+			<?php settings_errors(); ?>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields('glp_visualizer_option_group');
+					do_settings_sections('glp-visualizer-admin');
+					submit_button();
+				?>
+			</form>
+		</div>
+		<?php
+	}
+	
+	public function glp_visualizer_page_init() {
+		register_setting(
+			'glp_visualizer_option_group', // option_group
+			'glp_visualizer_option_name', // option_name
+			array($this, 'glp_visualizer_sanitize') // sanitize_callback
+		);
+
+		add_settings_section(
+			'glp_visualizer_setting_section', // id
+			'Settings', // title
+			array($this, 'glp_visualizer_section_info'), // callback
+			'glp-visualizer-admin' // page
+		);
+
+		add_settings_field(
+			'shiny_server', // id
+			'Shiny Server', // title
+			array($this, 'shiny_server_callback'), // callback
+			'glp-visualizer-admin', // page
+			'glp_visualizer_setting_section' // section
+		);
+		
+		add_settings_field(
+			'form_location', // id
+			'Questionnaire Location', // title
+			array( $this, 'form_location_callback' ), // callback
+			'glp-visualizer-admin', // page
+			'glp_visualizer_setting_section' // section
+		);
+	}
+	
+	public function glp_visualizer_sanitize($input) {
+		
+		$sanitary_values = array();
+		
+		if (isset( $input['shiny_server'])) {
+			$sanitary_values['shiny_server'] = sanitize_text_field($input['shiny_server']);
+		}
+		
+		if (isset( $input['form_location'])) {
+			$sanitary_values['form_location'] = sanitize_text_field($input['form_location']);
+		}
+
+		return $sanitary_values;
+	}
+	
+	public function glp_visualizer_section_info() {}
+	
+	public function shiny_server_callback() {
+		printf('<input class="regular-text" type="text" name="glp_visualizer_option_name[shiny_server]" id="shiny_server" value="%s">', isset($this->glp_visualizer_options['shiny_server']) ? esc_attr($this->glp_visualizer_options['shiny_server']) : '');
+	}
+	
+	public function form_location_callback() {
+		printf('<input class="regular-text" type="text" name="glp_visualizer_option_name[form_location]" id="form_location" value="%s">', isset($this->glp_visualizer_options['form_location']) ? esc_attr($this->glp_visualizer_options['form_location']) : '');
 	}
 	
 	private function loadScripts() {
